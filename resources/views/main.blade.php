fad403f20709145bf1ca3f72ea634a7d5a8c8ac5
sunsilver <jk05221@naver.com>
2020-12-20 00:40:53 +0900

    🎨 Add bootstrap template to blade

---
+++ resources/views/main.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+@section('content', 'title name')
+@section('style')
+
+@endsection
+
+@section( 'content')
+
+@endsection
+
+
+@section('script')
+<!-- Template Main JS File -->
+<script src="assets/js/main.js"></script>
+
+@endsection
